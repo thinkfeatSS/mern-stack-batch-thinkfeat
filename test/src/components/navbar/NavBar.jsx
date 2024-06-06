b41c832a608f31d74ca15498ffaee6cf504cd0b9
unknown <thinkfeat@gamil.com>
2024-06-06 19:01:04 +0500

    nav bar

--- test/src/components/navbar/NavBar.jsx
+++ test/src/components/navbar/NavBar.jsx
@@ -6,7 +6,7 @@ import LineIcon from "./LineIcon";
 // import icon from "/icon.png"
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-between ml-[30px] mr-[30px] sm:ml-8 sm:mr-8 md:ml-[65px] md:mr-[65px] pt-7 sm:pt-1">
+    <div className="flex items-center justify-between ml-[30px] mr-[30px] pt-7 sm:ml-8 sm:mr-8 sm:pt-1 md:ml-[65px] md:mr-[65px]">
       <Logo />
       <Navlinks/>
       <LineIcon/>
